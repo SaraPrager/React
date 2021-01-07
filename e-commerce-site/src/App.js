7deec620215498d0e67ac229afa989f6428ebe60
Sara Prager <sara_prager@tripplite.com>
2021-01-07 15:10:20 +0200

    Routing

--- e-commerce-site/src/App.js
+++ e-commerce-site/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <h1>
+    Hats Page
+  </h1>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
